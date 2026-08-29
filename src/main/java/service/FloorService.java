02dc5c5c2dfccfff50c4f2fee94de0a090cdbbfa
Abdullah22-22 <abdullahaltameemi28@gmail.com>
2026-08-29 11:51:50 +0300

    feat: add free space and profit calculation

---
+++ src/main/java/service/FloorService.java
@@ -0,0 +1,36 @@
+package service;
+
+public class FloorService {
+
+    // TODO: temporary. Delete when model/Floor.java is ready.
+        static class  Floor{
+            double area;
+            double services;
+
+            Floor(double area, double services){
+                this.area = area;
+                this.services = area;
+            }
+    }
+
+    public  double freeSpace(Floor floor){
+            if (floor.area <= 0){
+                throw new IllegalArgumentException("error.area.zero");
+            }
+            if (floor.services < 0){
+                throw new IllegalArgumentException("error.services.negative");
+            }
+            if (floor.services > floor.area) {
+                throw new IllegalArgumentException("error.services.big");
+            }
+            return floor.area - floor.services;
+    }
+
+    public double profit( double rentedArea, double rentPrice,double floorArea, double costPerM2){
+
+                double income = rentedArea * rentPrice;
+                double cost = floorArea * costPerM2;
+                return income - cost;
+    }
+
+}
