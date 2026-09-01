e17f889b1fd5d3d8bd68d5fb2c845de662ad9d74
Abdullah22-22 <abdullahaltameemi28@gmail.com>
2026-09-01 17:32:00 +0300

    fix: correct Floor constructor, remove profit for later
    services was assigned from area. profit moves to a later sprint.

--- src/main/java/service/FloorService.java
+++ src/main/java/service/FloorService.java
@@ -3,34 +3,26 @@ package service;
 public class FloorService {
 
     // TODO: temporary. Delete when model/Floor.java is ready.
-        static class  Floor{
-            double area;
-            double services;
+    static class Floor {
+        double area;
+        double services;
 
-            Floor(double area, double services){
-                this.area = area;
-                this.services = area;
-            }
+        Floor(double area, double services) {
+            this.area = area;
+            this.services = services;
+        }
     }
 
-    public  double freeSpace(Floor floor){
-            if (floor.area <= 0){
-                throw new IllegalArgumentException("error.area.zero");
-            }
-            if (floor.services < 0){
-                throw new IllegalArgumentException("error.services.negative");
-            }
-            if (floor.services > floor.area) {
-                throw new IllegalArgumentException("error.services.big");
-            }
-            return floor.area - floor.services;
+    public double freeSpace(Floor floor) {
+        if (floor.area <= 0) {
+            throw new IllegalArgumentException("error.area.zero");
+        }
+        if (floor.services < 0) {
+            throw new IllegalArgumentException("error.services.negative");
+        }
+        if (floor.services > floor.area) {
+            throw new IllegalArgumentException("error.services.big");
+        }
+        return floor.area - floor.services;
     }
-
-    public double profit( double rentedArea, double rentPrice,double floorArea, double costPerM2){
-
-                double income = rentedArea * rentPrice;
-                double cost = floorArea * costPerM2;
-                return income - cost;
-    }
-
 }
