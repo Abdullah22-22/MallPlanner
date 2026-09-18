e960dd81143bb253d5166b84153db883104af2cb
SarujanM <sarujan.mathyruban@gmail.com>
2026-09-18 12:13:39 +0300

    lisatty shop.java category

--- src/main/java/model/Shop.java
+++ src/main/java/model/Shop.java
@@ -6,19 +6,21 @@ public class Shop {
     private int floorId;
     private String name;
     private double area;
+    private String address;
 
-    public Shop(int id, int floorId, String name, double area) {
+    public Shop(int id, int floorId, String name, double area, String category) {
         this.id = id;
         this.floorId = floorId;
         this.name = name;
         this.area = area;
+        this.category = category;
     }
 
-    public Shop(int floorId, String name, double area) {
+    public Shop(int floorId, String name, double area, String category) {
         this.floorId = floorId;
         this.name = name;
         this.area = area;
-
+        this.category = category;
     }
 
     public int getId() { return id; }
@@ -32,4 +34,7 @@ public class Shop {
 
     public double getArea() { return area; }
     public void setArea(double area) { this.area = area; }
+
+    public String getCategory() { return category; }
+    public void setCategory(String category) { this.category = category; }
 }
